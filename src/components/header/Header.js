3f79fd6e9ce5b66e2848147d62bc6869b2ef0722
mahin1995 <mohimanul8@gmail.com>
2021-06-09 21:54:10 +0600

    project 5

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigations from "./Navigations";
+
+const Header = () => {
+  return (
+    <div>
+      <Navigations />
+    </div>
+  );
+};
+
+export default Header;
